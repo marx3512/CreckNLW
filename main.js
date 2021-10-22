ac875e0c2a80d67a4c19a238950ec93696342e34
Marx <83320377+marx3512@users.noreply.github.com>
2021-10-21 21:11:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+  github: 'marx3512',
+  youtube: 'channel/UCXkoaw5q2FwbVtLUWM023Mg',
+  instagram: 'marx478'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
